b401a72ab3a0377b38d3c8d8b335afeb2a73d882
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-10-09 18:44:41 +0530

    Add project files.

---
+++ HoroscopeApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HoroscopeApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
