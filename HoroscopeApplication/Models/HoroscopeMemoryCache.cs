4c893eec63caf15ac22b4fdb6374e2374d3f54e2
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-10-16 15:16:16 +0530

    Shifted from DB caching to in memory caching

---
+++ HoroscopeApplication/Models/HoroscopeMemoryCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoroscopeApplication.Models
+{
+    public class HoroscopeMemoryCache
+    {
+        [Required]
+        public Horoscope Horoscope { get; set; }
+        [Required]
+        public DateTime AddedTimestamp { get; set; }
+    }
+}
--- HoroscopeApplication/Repository/HoroscopeRepository.cs
+++ HoroscopeApplication/Repository/HoroscopeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HoroscopeApplication.Models;
 using HoroscopeApplication.Services;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,90 +12,158 @@ namespace HoroscopeApplication.Repository
         private readonly IHoroscopeApiService _horoscopeApiService;
         private readonly HoroscopeAppDbContext _dbContext;
 
+        private static readonly Dictionary<string, HoroscopeMemoryCache> _horoscopeCache = new Dictionary<string, HoroscopeMemoryCache>();
+
         public HoroscopeRepository(IHoroscopeApiService horoscopeApiService, 
                                     HoroscopeAppDbContext dbContext)
         {
             _horoscopeApiService = horoscopeApiService;
             _dbContext = dbContext;
         }
+
+        /**Working on DB cache**/
+        //public async Task<Horoscope> GetHoroscope(string sign)
+        //{
+        //    var prevHoroscope = _dbContext.HoroscopeCaches.FirstOrDefault(h => h.Sunsign.ToLower() == sign.ToLower());
+
+        //    Horoscope horoscopeModel = null;
+        //    if (prevHoroscope != null)
+        //    {
+        //        var currDate = DateTime.Now.Date;
+        //        var prevDate = prevHoroscope.AddedTimestamp.Date;
+
+        //        if (currDate > prevDate)
+        //        {
+        //            horoscopeModel = await _horoscopeApiService.GetHoroscope(sign);
+        //            RemoveCacheEntry(prevHoroscope);
+
+        //            HoroscopeCache horoscopeCache = new HoroscopeCache
+        //            {
+        //                Sunsign = sign,
+        //                Color = horoscopeModel.Color,
+        //                Compatibility = horoscopeModel.Compatibility,
+        //                Current_date = horoscopeModel.Current_date,
+        //                Date_range = horoscopeModel.Date_range,
+        //                Description = horoscopeModel.Description,
+        //                Lucky_number = horoscopeModel.Lucky_number,
+        //                Lucky_time = horoscopeModel.Lucky_time,
+        //                Mood = horoscopeModel.Mood,
+        //                AddedTimestamp = DateTime.Now
+        //            };
+        //            await AddCacheEntry(horoscopeCache);
+        //        }
+        //        else
+        //        {
+        //            horoscopeModel = new Horoscope
+        //            {
+        //                Color = prevHoroscope.Color,
+        //                Compatibility = prevHoroscope.Compatibility,
+        //                Current_date = prevHoroscope.Current_date,
+        //                Date_range = prevHoroscope.Date_range,
+        //                Description = prevHoroscope.Description,
+        //                Lucky_number = prevHoroscope.Lucky_number,
+        //                Lucky_time = prevHoroscope.Lucky_time,
+        //                Mood = prevHoroscope.Mood
+        //            };
+        //        }
+        //    }
+        //    else
+        //    {
+        //        horoscopeModel = await _horoscopeApiService.GetHoroscope(sign);
+        //        //_dbContext.HoroscopeCaches.Remove(prevHoroscope);
+
+        //        HoroscopeCache horoscopeCache = new HoroscopeCache
+        //        {
+        //            Sunsign = sign,
+        //            Color = horoscopeModel.Color,
+        //            Compatibility = horoscopeModel.Compatibility,
+        //            Current_date = horoscopeModel.Current_date,
+        //            Date_range = horoscopeModel.Date_range,
+        //            Description = horoscopeModel.Description,
+        //            Lucky_number = horoscopeModel.Lucky_number,
+        //            Lucky_time = horoscopeModel.Lucky_time,
+        //            Mood = horoscopeModel.Mood,
+        //            AddedTimestamp = DateTime.Now
+        //        };
+        //        await AddCacheEntry(horoscopeCache);
+        //    }
+
+        //    return horoscopeModel;
+        //}
+
+        //public async Task AddCacheEntry(HoroscopeCache horoscopeCache)
+        //{
+        //    await _dbContext.HoroscopeCaches.AddAsync(horoscopeCache);
+        //    await _dbContext.SaveChangesAsync();
+        //}
+
+        //public void RemoveCacheEntry(HoroscopeCache horoscopeCache)
+        //{
+        //    _dbContext.HoroscopeCaches.Remove(horoscopeCache);
+        //}
+
+
+
+
+
+        /**Working on In memory cache**/
         public async Task<Horoscope> GetHoroscope(string sign)
         {
-            var prevHoroscope = _dbContext.HoroscopeCaches.FirstOrDefault(h => h.Sunsign.ToLower() == sign.ToLower());
+            sign = sign.ToLower();
+            var prevHoroscope = _horoscopeCache.GetValueOrDefault(sign);
 
             Horoscope horoscopeModel = null;
             if (prevHoroscope != null)
             {
-                var currDate = DateTime.Now.Date;
+                var currDate = DateTime.UtcNow.AddDays(1).Date;
                 var prevDate = prevHoroscope.AddedTimestamp.Date;
 
                 if (currDate > prevDate)
                 {
                     horoscopeModel = await _horoscopeApiService.GetHoroscope(sign);
-                    RemoveCacheEntry(prevHoroscope);
+                    RemoveCacheEntry(sign);
 
-                    HoroscopeCache horoscopeCache = new HoroscopeCache
+                    HoroscopeMemoryCache horoscopeMemoryCache = new HoroscopeMemoryCache
                     {
-                        Sunsign = sign,
-                        Color = horoscopeModel.Color,
-                        Compatibility = horoscopeModel.Compatibility,
-                        Current_date = horoscopeModel.Current_date,
-                        Date_range = horoscopeModel.Date_range,
-                        Description = horoscopeModel.Description,
-                        Lucky_number = horoscopeModel.Lucky_number,
-                        Lucky_time = horoscopeModel.Lucky_time,
-                        Mood = horoscopeModel.Mood,
-                        AddedTimestamp = DateTime.Now
+                        Horoscope = horoscopeModel,
+                        AddedTimestamp = DateTime.UtcNow
                     };
-                    await AddCacheEntry(horoscopeCache);
+                    AddCacheEntry(sign, horoscopeMemoryCache);
                 }
                 else
                 {
-                    horoscopeModel = new Horoscope
-                    {
-                        Color = prevHoroscope.Color,
-                        Compatibility = prevHoroscope.Compatibility,
-                        Current_date = prevHoroscope.Current_date,
-                        Date_range = prevHoroscope.Date_range,
-                        Description = prevHoroscope.Description,
-                        Lucky_number = prevHoroscope.Lucky_number,
-                        Lucky_time = prevHoroscope.Lucky_time,
-                        Mood = prevHoroscope.Mood
-                    };
+                    horoscopeModel = prevHoroscope.Horoscope;
                 }
             }
             else
             {
                 horoscopeModel = await _horoscopeApiService.GetHoroscope(sign);
-                //_dbContext.HoroscopeCaches.Remove(prevHoroscope);
 
-                HoroscopeCache horoscopeCache = new HoroscopeCache
+                HoroscopeMemoryCache horoscopeMemoryCache = new HoroscopeMemoryCache
                 {
-                    Sunsign = sign,
-                    Color = horoscopeModel.Color,
-                    Compatibility = horoscopeModel.Compatibility,
-                    Current_date = horoscopeModel.Current_date,
-                    Date_range = horoscopeModel.Date_range,
-                    Description = horoscopeModel.Description,
-                    Lucky_number = horoscopeModel.Lucky_number,
-                    Lucky_time = horoscopeModel.Lucky_time,
-                    Mood = horoscopeModel.Mood,
-                    AddedTimestamp = DateTime.Now
+                    Horoscope = horoscopeModel,
+                    AddedTimestamp = DateTime.UtcNow
                 };
-                await AddCacheEntry(horoscopeCache);
+                AddCacheEntry(sign, horoscopeMemoryCache);
             }
 
             return horoscopeModel;
         }
 
-        public async Task AddCacheEntry(HoroscopeCache horoscopeCache)
+        private void AddCacheEntry(string sign, HoroscopeMemoryCache horoscopeMemoryCache)
         {
-            await _dbContext.HoroscopeCaches.AddAsync(horoscopeCache);
-            await _dbContext.SaveChangesAsync();
+            if (!_horoscopeCache.ContainsKey(sign))
+            {
+                _horoscopeCache.Add(sign, horoscopeMemoryCache);
+            }
         }
 
-        public void RemoveCacheEntry(HoroscopeCache horoscopeCache)
+        private void RemoveCacheEntry(string sign)
         {
-            _dbContext.HoroscopeCaches.Remove(horoscopeCache);
+            if (_horoscopeCache.ContainsKey(sign))
+            {
+                _horoscopeCache.Remove(sign);
+            }
         }
     }
 }
